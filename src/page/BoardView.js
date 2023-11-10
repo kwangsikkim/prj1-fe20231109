4cff2da1adbb987e681135244d4fb38b5956d425
kwangsik Kim <penggusic@gmail.com>
2023-11-10 09:23:10 +0900

    수정 버튼 클릭 시 수정화면(/edit/:id)으로 이동

--- src/page/BoardView.js
+++ src/page/BoardView.js
@@ -80,7 +80,9 @@ export function BoardView() {
         <Input value={board.inserted} readOnly />
       </FormControl>
 
-      <Button colorScheme="purple">수정</Button>
+      <Button colorScheme="purple" onClick={() => navigate("/edit/" + id)}>
+        수정
+      </Button>
       <Button colorScheme="red" onClick={onOpen}>
         삭제
       </Button>
