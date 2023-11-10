72155fd43f65a3d2db7edc36b2c1e72025b7c9ed
kwangsik Kim <penggusic@gmail.com>
2023-11-10 11:37:55 +0900

    저장 시 모달로 확인

--- src/page/BoardEdit.js
+++ src/page/BoardEdit.js
@@ -4,24 +4,33 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Spinner,
   Textarea,
+  useDisclosure,
   useToast,
 } from "@chakra-ui/react";
 import React, { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useImmer } from "use-immer";
 import axios from "axios";
+import { logDOM } from "@testing-library/react";
 
 export function BoardEdit() {
   const [board, updateBoard] = useImmer(null);
 
   // /edit/:id
   const { id } = useParams();
-
   const navigate = useNavigate();
-
   const toast = useToast();
+
+  const { isOpen, onClose, onOpen } = useDisclosure();
 
   useEffect(() => {
     axios
@@ -41,69 +50,86 @@ export function BoardEdit() {
       .put("/api/board/edit", board)
       .then(() => {
         toast({
-          description: board.id + "번 글이 수정되었습니다",
+          description: board.id + "번 게시글이 수정되었습니다.",
           status: "success",
         });
+
         navigate("/board/" + id);
       })
       .catch((error) => {
         if (error.response.status === 400) {
           toast({
-            description: "요청이 잘못되었습니다",
+            description: "요청이 잘못되었습니다.",
             status: "error",
           });
         } else {
           toast({
-            description: "수정 중에 문제가 발생하였습니다",
+            description: "수정 중에 문제가 발생하였습니다.",
             status: "error",
           });
         }
       })
-      .finally(() => console.log("끝"));
+      .finally(() => onClose());
   }
 
   return (
     <Box>
       <h1>{id}번 글 수정</h1>
-
       <FormControl>
         <FormLabel>제목</FormLabel>
         <Input
           value={board.title}
-          onChange={(e) => {
+          onChange={(e) =>
             updateBoard((draft) => {
               draft.title = e.target.value;
-            });
-          }}
+            })
+          }
         />
       </FormControl>
       <FormControl>
         <FormLabel>본문</FormLabel>
         <Textarea
           value={board.content}
-          onChange={(e) => {
+          onChange={(e) =>
             updateBoard((draft) => {
               draft.content = e.target.value;
-            });
-          }}
+            })
+          }
         />
       </FormControl>
       <FormControl>
         <FormLabel>작성자</FormLabel>
         <Input
           value={board.writer}
-          onChange={(e) => {
+          onChange={(e) =>
             updateBoard((draft) => {
               draft.writer = e.target.value;
-            });
-          }}
+            })
+          }
         />
       </FormControl>
-      <Button colorScheme="blue" onClick={handleSubmit}>
+      <Button colorScheme="blue" onClick={onOpen}>
         저장
       </Button>
-      {/* navigate(-1) : 이전 경로 이동 */}
+      {/* navigate(-1) : 이전 경로로 이동 */}
       <Button onClick={() => navigate(-1)}>취소</Button>
+
+      {/* 수정 모달 */}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>저장 확인</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>저장 하시겠습니까?</ModalBody>
+
+          <ModalFooter>
+            <Button onClick={onClose}>닫기</Button>
+            <Button onClick={handleSubmit} colorScheme="blue">
+              저장
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
