4a429dc3e960067cd77b1bc9201098501fcdb4da
kwangsik Kim <penggusic@gmail.com>
2023-11-15 14:22:59 +0900

    댓글 삭제 후 리스트 업데이트 기능 추가

--- src/component/CommentContainer.js
+++ src/component/CommentContainer.js
@@ -30,13 +30,7 @@ function CommentForm({ boardId, isSubmitting, onSubmit }) {
   );
 }
 
-function CommentList({ commentList }) {
-  function handleDelete(id) {
-    // console.log(id + "번 댓삭");
-    // TODO: then, catch, finally
-    axios.delete("/api/comment/" + id);
-  }
-
+function CommentList({ commentList, onDelete, isSubmitting }) {
   return (
     <Card>
       <CardHeader>
@@ -56,7 +50,8 @@ function CommentList({ commentList }) {
                   {comment.comment}
                 </Text>
                 <Button
-                  onClick={() => handleDelete(comment.id)}
+                  isDisabled={isSubmitting}
+                  onClick={() => onDelete(comment.id)}
                   size="xs"
                   colorScheme="red"
                 >
@@ -95,6 +90,14 @@ export function CommentContainer({ boardId }) {
       .finally(() => setIsSubmitting(false));
   }
 
+  function handleDelete(id) {
+    // console.log(id + "번 댓삭");
+    // TODO: then, catch, finally
+    setIsSubmitting(true);
+
+    axios.delete("/api/comment/" + id).finally(() => setIsSubmitting(false));
+  }
+
   return (
     <Box>
       <CommentForm
@@ -102,7 +105,12 @@ export function CommentContainer({ boardId }) {
         isSubmitting={isSubmitting}
         onSubmit={handleSubmit}
       />
-      <CommentList boardId={boardId} commentList={commentList} />
+      <CommentList
+        boardId={boardId}
+        isSubmitting={isSubmitting}
+        commentList={commentList}
+        onDelete={handleDelete}
+      />
     </Box>
   );
 }
