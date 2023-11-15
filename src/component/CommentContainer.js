6c16f6d970f96f0a75e872b7d63e614a7adadf12
kwangsik Kim <penggusic@gmail.com>
2023-11-15 12:13:30 +0900

    댓글 리스트 key prop 추가

--- src/component/CommentContainer.js
+++ src/component/CommentContainer.js
@@ -39,7 +39,7 @@ function CommentList({ commentList }) {
         <Stack divider={<StackDivider />} spacing="4">
           {/* TODO: 댓글 작성 후 re render */}
           {commentList.map((comment) => (
-            <Box>
+            <Box key={comment.id}>
               <Flex justifyContent="space-between">
                 <Heading size="xs">{comment.memberId}</Heading>
                 <Text fontSize="xs">{comment.inserted}</Text>
