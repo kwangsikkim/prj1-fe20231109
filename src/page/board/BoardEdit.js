9fb95b1827f25093d8fec4c29ba90c60b71e30dc
kwangsik Kim <penggusic@gmail.com>
2023-11-20 09:54:44 +0900

    검색 Input, Button 추가 / 검색 후 페이지 이동 기능 추가

--- src/page/board/BoardEdit.js
+++ src/page/board/BoardEdit.js
@@ -1,134 +1,125 @@
-import React, { useEffect, useState } from "react";
 import {
-  Badge,
   Box,
   Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Spinner,
-  Table,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
+  Textarea,
+  useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useImmer } from "use-immer";
 import axios from "axios";
-import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
-import { ChatIcon } from "@chakra-ui/icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faAngleLeft,
-  faAngleRight,
-  faHeart,
-} from "@fortawesome/free-solid-svg-icons";
+import { logDOM } from "@testing-library/react";
 
-function Pagination({ pageInfo }) {
-  const pageNumbers = [];
+export function BoardEdit() {
+  const [board, updateBoard] = useImmer(null);
 
+  // /edit/:id
+  const { id } = useParams();
   const navigate = useNavigate();
+  const toast = useToast();
 
-  for (let i = pageInfo.startPageNumber; i <= pageInfo.endPageNumber; i++) {
-    pageNumbers.push(i);
-  }
-
-  return (
-    <Box>
-      {pageInfo.prevPageNumber && (
-        <Button
-          variant="ghost"
-          onClick={() => navigate("/?p=" + pageInfo.prevPageNumber)}
-        >
-          <FontAwesomeIcon icon={faAngleLeft} />
-        </Button>
-      )}
-
-      {pageNumbers.map((pageNumber) => (
-        <Button
-          key={pageNumber}
-          variant={
-            pageNumber === pageInfo.currentPageNumber ? "solid" : "ghost"
-          }
-          onClick={() => navigate("/?p=" + pageNumber)}
-        >
-          {pageNumber}
-        </Button>
-      ))}
-
-      {pageInfo.nextPageNumber && (
-        <Button
-          variant="ghost"
-          onClick={() => navigate("/?p=" + pageInfo.nextPageNumber)}
-        >
-          <FontAwesomeIcon icon={faAngleRight} />
-        </Button>
-      )}
-    </Box>
-  );
-}
-
-export function BoardList() {
-  const [boardList, setBoardList] = useState(null);
-  const [pageInfo, setPageInfo] = useState(null);
-
-  const [params] = useSearchParams();
-  const navigate = useNavigate();
-  const location = useLocation();
+  const { isOpen, onClose, onOpen } = useDisclosure();
 
   useEffect(() => {
-    axios.get("/api/board/list?" + params).then((response) => {
-      setBoardList(response.data.boardList);
-      setPageInfo(response.data.pageInfo);
-    });
-  }, [location]);
+    axios
+      .get("/api/board/id/" + id)
+      .then((response) => updateBoard(response.data));
+  }, []);
 
-  if (boardList === null) {
+  if (board === null) {
     return <Spinner />;
   }
 
+  function handleSubmit() {
+    // 저장 버튼 클릭 시
+    // PUT /api/board/edit
+
+    axios
+      .put("/api/board/edit", board)
+      .then(() => {
+        toast({
+          description: board.id + "번 게시글이 수정되었습니다.",
+          status: "success",
+        });
+
+        navigate("/board/" + id);
+      })
+      .catch((error) => {
+        if (error.response.status === 400) {
+          toast({
+            description: "요청이 잘못되었습니다.",
+            status: "error",
+          });
+        } else {
+          toast({
+            description: "수정 중에 문제가 발생하였습니다.",
+            status: "error",
+          });
+        }
+      })
+      .finally(() => onClose());
+  }
+
   return (
     <Box>
-      <h1>게시물 목록</h1>
-      <Box>
-        <Table>
-          <Thead>
-            <Tr>
-              <Th>id</Th>
-              <Th>
-                <FontAwesomeIcon icon={faHeart} />
-              </Th>
-              <Th>title</Th>
-              <Th>by</Th>
-              <Th>at</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {boardList.map((board) => (
-              <Tr
-                _hover={{
-                  cursor: "pointer",
-                }}
-                key={board.id}
-                onClick={() => navigate("/board/" + board.id)}
-              >
-                <Td>{board.id}</Td>
-                <Td>{board.countLike != 0 && board.countLike}</Td>
-                <Td>
-                  {board.title}
-                  {board.countComment > 0 && (
-                    <Badge>
-                      <ChatIcon />
-                      {board.countComment}
-                    </Badge>
-                  )}
-                </Td>
-                <Td>{board.nickName}</Td>
-                <Td>{board.ago}</Td>
-              </Tr>
-            ))}
-          </Tbody>
-        </Table>
-      </Box>
+      <h1>{id}번 글 수정</h1>
+      <FormControl>
+        <FormLabel>제목</FormLabel>
+        <Input
+          value={board.title}
+          onChange={(e) =>
+            updateBoard((draft) => {
+              draft.title = e.target.value;
+            })
+          }
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel>본문</FormLabel>
+        <Textarea
+          value={board.content}
+          onChange={(e) =>
+            updateBoard((draft) => {
+              draft.content = e.target.value;
+            })
+          }
+        />
+      </FormControl>
 
-      <Pagination pageInfo={pageInfo} />
+      <Button colorScheme="blue" onClick={onOpen}>
+        저장
+      </Button>
+      {/* navigate(-1) : 이전 경로로 이동 */}
+      <Button onClick={() => navigate(-1)}>취소</Button>
+
+      {/* 수정 모달 */}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>저장 확인</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>저장 하시겠습니까?</ModalBody>
+
+          <ModalFooter>
+            <Button onClick={onClose}>닫기</Button>
+            <Button onClick={handleSubmit} colorScheme="blue">
+              저장
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
--- src/page/board/BoardList.js
+++ src/page/board/BoardList.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import {
   Badge,
   Box,
+  Button,
+  Flex,
+  Input,
   Spinner,
   Table,
   Tbody,
@@ -11,21 +14,110 @@ import {
   Tr,
 } from "@chakra-ui/react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 import { ChatIcon } from "@chakra-ui/icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAngleLeft,
+  faAngleRight,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
+import * as PropTypes from "prop-types";
 
-export function BoardList() {
-  const [boardList, setBoardList] = useState(null);
+function PageButton({ variant, pageNumber, children }) {
+  const [params] = useSearchParams();
+  const navigate = useNavigate();
+
+  function handleClick() {
+    params.set("p", pageNumber);
+    navigate("/?" + params);
+  }
+
+  return (
+    <Button variant={variant} onClick={handleClick}>
+      {children}
+    </Button>
+  );
+}
+
+function Pagination({ pageInfo }) {
+  const pageNumbers = [];
 
   const navigate = useNavigate();
 
+  for (let i = pageInfo.startPageNumber; i <= pageInfo.endPageNumber; i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <Box>
+      {pageInfo.prevPageNumber && (
+        <Button
+          variant="ghost"
+          onClick={() => navigate("/?p=" + pageInfo.prevPageNumber)}
+        >
+          <FontAwesomeIcon icon={faAngleLeft} />
+        </Button>
+      )}
+
+      {pageNumbers.map((pageNumber) => (
+        <PageButton
+          key={pageNumber}
+          variant={
+            pageNumber === pageInfo.currentPageNumber ? "solid" : "ghost"
+          }
+          pageNumber={pageNumber}
+        >
+          {pageNumber}
+        </PageButton>
+      ))}
+
+      {pageInfo.nextPageNumber && (
+        <Button
+          variant="ghost"
+          onClick={() => navigate("/?p=" + pageInfo.nextPageNumber)}
+        >
+          <FontAwesomeIcon icon={faAngleRight} />
+        </Button>
+      )}
+    </Box>
+  );
+}
+
+function SearchComponent() {
+  const [keyword, setKeyword] = useState("");
+  const navigate = useNavigate();
+
+  function handleSearch() {
+    // /?k=keyword
+    const params = new URLSearchParams();
+    params.set("k", keyword);
+
+    navigate("/?" + params);
+  }
+
+  return (
+    <Flex>
+      <Input value={keyword} onChange={(e) => setKeyword(e.target.value)} />
+      <Button onClick={handleSearch}>검색</Button>
+    </Flex>
+  );
+}
+
+export function BoardList() {
+  const [boardList, setBoardList] = useState(null);
+  const [pageInfo, setPageInfo] = useState(null);
+
+  const [params] = useSearchParams();
+  const navigate = useNavigate();
+  const location = useLocation();
+
   useEffect(() => {
-    axios
-      .get("/api/board/list")
-      .then((response) => setBoardList(response.data));
-  }, []);
+    axios.get("/api/board/list?" + params).then((response) => {
+      setBoardList(response.data.boardList);
+      setPageInfo(response.data.pageInfo);
+    });
+  }, [location]);
 
   if (boardList === null) {
     return <Spinner />;
@@ -68,12 +160,15 @@ export function BoardList() {
                   )}
                 </Td>
                 <Td>{board.nickName}</Td>
-                <Td>{board.inserted}</Td>
+                <Td>{board.ago}</Td>
               </Tr>
             ))}
           </Tbody>
         </Table>
       </Box>
+
+      <SearchComponent />
+      <Pagination pageInfo={pageInfo} />
     </Box>
   );
 }
